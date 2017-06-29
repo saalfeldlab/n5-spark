f454130313f81a80d6b4dc24da06a59db5d6a120
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-06-29 16:21:40 -0400

    add parallel remove for large n5 datasets

---
+++ src/main/java/org/janelia/saalfeldlab/n5/spark/N5RemoveSpark.java
@@ -0,0 +1,44 @@
+package org.janelia.saalfeldlab.n5.spark;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.janelia.saalfeldlab.n5.N5;
+import org.janelia.saalfeldlab.n5.N5Writer;
+
+public class N5RemoveSpark
+{
+	private final transient JavaSparkContext sparkContext;
+
+	public N5RemoveSpark( final JavaSparkContext sparkContext )
+	{
+		this.sparkContext = sparkContext;
+	}
+
+	public void remove( final String basePath, final String pathName ) throws IOException
+	{
+		final N5Writer n5 = N5.openFSWriter( basePath );
+		final List< String > leaves = new ArrayList<>();
+		final Queue< String > nodesQueue = new LinkedList<>();
+		nodesQueue.add( pathName );
+		while ( !nodesQueue.isEmpty() )
+		{
+			final String node = nodesQueue.remove();
+			final String[] children = n5.list( node );
+			if ( children.length == 0 )
+				leaves.add( node );
+			else
+				for ( final String child : children )
+					nodesQueue.add( Paths.get( node, child ).toString() );
+		}
+
+		sparkContext.parallelize( leaves ).foreach( leaf -> N5.openFSWriter( basePath ).remove( leaf ) );
+
+		n5.remove( pathName );
+	}
+}
