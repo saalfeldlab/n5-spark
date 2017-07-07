6c29b97119f2283934beb184215dcbddd57fc45c
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-07-07 19:03:36 -0400

    fix n5 slice tiff converter implementation
    - use bounded cache of size 1 to keep memory usage low and prevent tasks from failing
    - work with cells directly instead of using high-level imglib2 iterable view because it does not preserve CellIterationOrder

--- src/main/java/org/janelia/saalfeldlab/n5/spark/N5SliceTiffConverter.java
+++ src/main/java/org/janelia/saalfeldlab/n5/spark/N5SliceTiffConverter.java
@@ -2,22 +2,29 @@ package org.janelia.saalfeldlab.n5.spark;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 import org.apache.spark.api.java.JavaSparkContext;
 import org.janelia.saalfeldlab.n5.DatasetAttributes;
 import org.janelia.saalfeldlab.n5.N5;
 import org.janelia.saalfeldlab.n5.N5Reader;
-import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 
 import ij.IJ;
 import ij.ImagePlus;
 import net.imglib2.Cursor;
+import net.imglib2.FinalInterval;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.cache.img.CachedCellImg;
+import net.imglib2.img.cell.Cell;
+import net.imglib2.img.cell.CellGrid;
+import net.imglib2.img.cell.CellRandomAccess;
+import net.imglib2.img.cell.LazyCellImg.LazyCells;
 import net.imglib2.img.imageplus.ImagePlusImg;
 import net.imglib2.img.imageplus.ImagePlusImgFactory;
+import net.imglib2.iterator.IntervalIterator;
 import net.imglib2.type.NativeType;
 import net.imglib2.util.Util;
 import net.imglib2.view.Views;
@@ -34,27 +41,40 @@ public class N5SliceTiffConverter
 		final DatasetAttributes attributes = n5.getDatasetAttributes( datasetPath );
 		final long[] dimensions = attributes.getDimensions();
 
-		final long[] sliceDimensions = new long[] { dimensions[ 0 ], dimensions[ 1 ] };
-		final List< Integer > zCoords = new ArrayList<>();
-		for ( int z = 0; z < dimensions[ 2 ]; ++z )
-			zCoords.add( z );
+		final List< Long > zCoords = LongStream.range( 0, dimensions[ 2 ] ).boxed().collect( Collectors.toList() );
 
 		Paths.get( outputPath ).toFile().mkdirs();
 
 		sparkContext.parallelize( zCoords, zCoords.size() ).foreach( z ->
 			{
 				final N5Reader n5Local = N5.openFSReader( basePath );
-				final RandomAccessibleInterval< T > img = N5Utils.open( n5Local, datasetPath );
-				final RandomAccessibleInterval< T > sliceSource = Views.hyperSlice( img, 2, z );
-				final ImagePlusImg< T, ? > sliceTarget = new ImagePlusImgFactory< T >().create( sliceDimensions, Util.getTypeFromInterval( sliceSource ) );
-				final Cursor< T > sliceSourceCursor = Views.iterable( sliceSource ).localizingCursor();
-				final RandomAccess< T > sliceTargetRandomAccess = sliceTarget.randomAccess();
-				while ( sliceSourceCursor.hasNext() )
+				final CachedCellImg< T, ? > cellImg = N5SparkUtils.openWithBoundedCache( n5Local, datasetPath, 1 );
+				final CellRandomAccess< T, ? extends Cell< ? > > cellImgRandomAccess = cellImg.randomAccess();
+				final CellGrid cellGrid = cellImg.getCellGrid();
+				final long[] zCellPos = new long[ cellImg.numDimensions() ];
+				cellGrid.getCellPosition( new long[] { 0, 0, z }, zCellPos );
+
+				final ImagePlusImg< T, ? > sliceTarget = new ImagePlusImgFactory< T >().create( new long[] { dimensions[ 0 ], dimensions[ 1 ] }, Util.getTypeFromInterval( cellImg ) );
+				final RandomAccessibleInterval< T > target = Views.translate( Views.stack( sliceTarget ), new long[] { 0, 0, z } );
+				final RandomAccess< T > targetRandomAccess = target.randomAccess();
+
+				final LazyCells< ? extends Cell< ? > > cells = cellImg.getCells();
+				final RandomAccessibleInterval< ? extends Cell< ? > > sliceCells = Views.interval( cells, new FinalInterval( new long[] { cells.min( 0 ), cells.min( 1 ), zCellPos[ 2 ] }, new long[] { cells.max( 0 ), cells.max( 1 ), zCellPos[ 2 ] } ) );
+				final Cursor< ? extends Cell< ? > > sliceCellsCursor = Views.iterable( sliceCells ).cursor();
+
+				while ( sliceCellsCursor.hasNext() )
 				{
-					sliceSourceCursor.fwd();
-					sliceTargetRandomAccess.setPosition( sliceSourceCursor );
-					sliceTargetRandomAccess.get().set( sliceSourceCursor.get() );
+					final Cell< ? > cell = sliceCellsCursor.next();
+					final IntervalIterator cellSliceIterator = IntervalIterator.create( new FinalInterval( new long[] { cell.min( 0 ), cell.min( 1 ), z }, new long[] { cell.min( 0 ) + cell.dimension( 0 ) - 1, cell.min( 1 ) + cell.dimension( 1 ) - 1, z } ) );
+					while ( cellSliceIterator.hasNext() )
+					{
+						cellSliceIterator.fwd();
+						cellImgRandomAccess.setPosition( cellSliceIterator );
+						targetRandomAccess.setPosition( cellSliceIterator );
+						targetRandomAccess.get().set( cellImgRandomAccess.get() );
+					}
 				}
+
 				final ImagePlus sliceImp = sliceTarget.getImagePlus();
 				IJ.saveAsTiff( sliceImp, Paths.get( outputPath, z + ".tif" ).toString() );
 			} );
---
+++ src/main/java/org/janelia/saalfeldlab/n5/spark/N5SparkUtils.java
@@ -0,0 +1,129 @@
+package org.janelia.saalfeldlab.n5.spark;
+
+import static net.imglib2.cache.img.PrimitiveType.BYTE;
+import static net.imglib2.cache.img.PrimitiveType.DOUBLE;
+import static net.imglib2.cache.img.PrimitiveType.FLOAT;
+import static net.imglib2.cache.img.PrimitiveType.INT;
+import static net.imglib2.cache.img.PrimitiveType.LONG;
+import static net.imglib2.cache.img.PrimitiveType.SHORT;
+
+import java.io.IOException;
+
+import org.janelia.saalfeldlab.n5.DatasetAttributes;
+import org.janelia.saalfeldlab.n5.N5Reader;
+import org.janelia.saalfeldlab.n5.imglib2.N5CellLoader;
+
+import net.imglib2.cache.Cache;
+import net.imglib2.cache.CacheLoader;
+import net.imglib2.cache.LoaderCache;
+import net.imglib2.cache.img.ArrayDataAccessFactory;
+import net.imglib2.cache.img.CachedCellImg;
+import net.imglib2.cache.img.LoadedCellCacheLoader;
+import net.imglib2.cache.ref.BoundedSoftRefLoaderCache;
+import net.imglib2.img.cell.Cell;
+import net.imglib2.img.cell.CellGrid;
+import net.imglib2.img.cell.LazyCellImg;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.integer.ByteType;
+import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.type.numeric.integer.LongType;
+import net.imglib2.type.numeric.integer.ShortType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.integer.UnsignedIntType;
+import net.imglib2.type.numeric.integer.UnsignedLongType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.type.numeric.real.FloatType;
+
+public class N5SparkUtils
+{
+	private N5SparkUtils() { }
+
+	/**
+	 * Open an N5 dataset as a memory cached {@link LazyCellImg} with bounded cache size.
+	 *
+	 * @param n5
+	 * @param dataset
+	 * @param cacheSize
+	 * @return
+	 * @throws IOException
+	 */
+	@SuppressWarnings( { "unchecked", "rawtypes" } )
+	public static final < T extends NativeType< T > > CachedCellImg< T, ? > openWithBoundedCache(
+			final N5Reader n5,
+			final String dataset,
+			final int cacheSize ) throws IOException
+	{
+		final DatasetAttributes attributes = n5.getDatasetAttributes( dataset );
+		final long[] dimensions = attributes.getDimensions();
+		final int[] blockSize = attributes.getBlockSize();
+
+		final N5CellLoader< T > loader = new N5CellLoader<>( n5, dataset, blockSize );
+
+		final CellGrid grid = new CellGrid( dimensions, blockSize );
+
+		final CachedCellImg< T, ? > img;
+		final T type;
+		final Cache< Long, Cell< ? > > cache;
+
+		final LoaderCache< Long, Cell< ? > > loaderCache = new BoundedSoftRefLoaderCache<>( cacheSize );
+
+		switch ( attributes.getDataType() )
+		{
+		case INT8:
+			type = ( T )new ByteType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( BYTE ) );
+			break;
+		case UINT8:
+			type = ( T )new UnsignedByteType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( BYTE ) );
+			break;
+		case INT16:
+			type = ( T )new ShortType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( SHORT ) );
+			break;
+		case UINT16:
+			type = ( T )new UnsignedShortType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( SHORT ) );
+			break;
+		case INT32:
+			type = ( T )new IntType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( INT ) );
+			break;
+		case UINT32:
+			type = ( T )new UnsignedIntType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( INT ) );
+			break;
+		case INT64:
+			type = ( T )new LongType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( LONG ) );
+			break;
+		case UINT64:
+			type = ( T )new UnsignedLongType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( LONG ) );
+			break;
+		case FLOAT32:
+			type = ( T )new FloatType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( FLOAT ) );
+			break;
+		case FLOAT64:
+			type = ( T )new DoubleType();
+			cache = loaderCache.withLoader( ( CacheLoader )LoadedCellCacheLoader.get( grid, loader, type ) );
+			img = new CachedCellImg( grid, type, cache, ArrayDataAccessFactory.get( DOUBLE ) );
+			break;
+		default:
+			img = null;
+		}
+
+		return img;
+	}
+}
