24ec5eb26b673932e956acbb34d5df81d61e8228
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-02-20 19:59:23 -0500

    increase cluster walltime to 24h for n5 converter

--- startup-scripts/spark-janelia/n5-convert.py
+++ startup-scripts/spark-janelia/n5-convert.py
@@ -16,6 +16,7 @@ flintstone_path = os.path.join(curr_script_dir, flintstone_relpath)
 os.environ['SPARK_VERSION'] = 'test'
 os.environ['N_DRIVER_THREADS'] = '2'
 os.environ['MEMORY_PER_NODE'] = '115'
+os.environ['RUNTIME'] = '24:00'
 os.environ['TERMINATE'] = '1'
 
 nodes = int(sys.argv[1])
