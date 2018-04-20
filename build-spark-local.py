90ebdbcf1250e437f19ee1be2c82cb0deaa4742e
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-04-20 19:03:46 -0400

    rename compiled jar to avoid overwriting in eclipse

--- build-spark-local.py
+++ build-spark-local.py
@@ -2,7 +2,6 @@
 
 import os
 import sys
-
 sys.dont_write_bytecode = True
 from build import run_build
 
@@ -10,3 +9,8 @@ if __name__ == '__main__':
 	base_folder = os.path.dirname(os.path.abspath(__file__))
 	build_args = ['-P', 'fatjar,spark-local']
 	run_build(base_folder, build_args)
+	
+	curr_script_dir = os.path.dirname(os.path.abspath(__file__))
+	sys.path.append(os.path.join(curr_script_dir, 'startup-scripts'))
+	from jar_path_util import get_jar_path, get_local_jar_path
+	os.rename(get_jar_path(), get_local_jar_path())
--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import subprocess
+sys.dont_write_bytecode = True
 
 def run_build(base_folder, build_args=[]):
 	cmd_args = ['mvn', 'clean', 'package'] + build_args
@@ -11,3 +13,8 @@ if __name__ == '__main__':
 	base_folder = os.path.dirname(os.path.abspath(__file__))
 	build_args = ['-P', 'fatjar,spark-provided']
 	run_build(base_folder, build_args)
+
+	curr_script_dir = os.path.dirname(os.path.abspath(__file__))
+	sys.path.append(os.path.join(curr_script_dir, 'startup-scripts'))
+	from jar_path_util import get_jar_path, get_provided_jar_path
+	os.rename(get_jar_path(), get_provided_jar_path())
