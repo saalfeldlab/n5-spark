a381ab3fc8401ad0beb6d47af34e2f7734816392
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-06-01 13:46:20 -0400

    simplify jar renaming in build scripts

--- build-spark-local.py
+++ build-spark-local.py
@@ -3,14 +3,10 @@
 import os
 import sys
 sys.dont_write_bytecode = True
-from build import run_build
+from build import run_build, rename_jar
 
 if __name__ == '__main__':
 	base_folder = os.path.dirname(os.path.abspath(__file__))
 	build_args = ['-P', 'fatjar,spark-local']
 	run_build(base_folder, build_args)
-	
-	curr_script_dir = os.path.dirname(os.path.abspath(__file__))
-	sys.path.append(os.path.join(curr_script_dir, 'startup-scripts'))
-	from jar_path_util import get_jar_path, get_local_jar_path
-	os.rename(get_jar_path(), get_local_jar_path())
+	rename_jar('-local')
--- build.py
+++ build.py
@@ -9,12 +9,14 @@ def run_build(base_folder, build_args=[]):
 	cmd_args = ['mvn', 'package'] + build_args
 	subprocess.call(cmd_args, cwd=base_folder)
 
+def rename_jar(new_jar_suffix):
+	curr_script_dir = os.path.dirname(os.path.abspath(__file__))
+	sys.path.append(os.path.join(curr_script_dir, 'startup-scripts'))
+	from jar_path_util import get_jar_path
+	os.rename(get_jar_path(), get_jar_path(new_jar_suffix))
+
 if __name__ == '__main__':
 	base_folder = os.path.dirname(os.path.abspath(__file__))
 	build_args = ['-P', 'fatjar,spark-provided']
 	run_build(base_folder, build_args)
-
-	curr_script_dir = os.path.dirname(os.path.abspath(__file__))
-	sys.path.append(os.path.join(curr_script_dir, 'startup-scripts'))
-	from jar_path_util import get_jar_path, get_provided_jar_path
-	os.rename(get_jar_path(), get_provided_jar_path())
+	rename_jar('-provided')
