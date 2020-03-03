4265a555b2494946f0083a883f7745347c87835b
Igor Pisarev <pisarevi@janelia.hhmi.org>
2020-03-03 10:25:26 -0500

    update connected components test to use new abstract test class

--- src/test/java/org/janelia/saalfeldlab/n5/spark/N5ConnectedComponentsSparkTest.java
+++ src/test/java/org/janelia/saalfeldlab/n5/spark/N5ConnectedComponentsSparkTest.java
@@ -7,58 +7,22 @@ import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.type.numeric.integer.UnsignedLongType;
 import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.janelia.saalfeldlab.n5.GzipCompression;
 import org.janelia.saalfeldlab.n5.N5FSWriter;
 import org.janelia.saalfeldlab.n5.N5Writer;
 import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
-import org.janelia.saalfeldlab.n5.spark.supplier.N5WriterSupplier;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public class N5ConnectedComponentsSparkTest
+public class N5ConnectedComponentsSparkTest extends AbstractN5SparkTest
 {
-    static private final String basePath = System.getProperty("user.home") + "/.n5-spark-test-" + RandomStringUtils.randomAlphanumeric(5);
     static private final String datasetPath = "data";
     static private final String relabeledDatasetPath = "data-relabeled";
-
-    static private final N5WriterSupplier n5Supplier = () -> new N5FSWriter( basePath );
-
-    private JavaSparkContext sparkContext;
-
-    @Before
-    public void setUp() throws IOException
-    {
-        // cleanup in case the test has failed
-        tearDown();
-
-        sparkContext = new JavaSparkContext( new SparkConf()
-                .setMaster( "local[*]" )
-                .setAppName( "N5ConnectedComponentsTest" )
-                .set( "spark.serializer", "org.apache.spark.serializer.KryoSerializer" )
-        );
-    }
-
-    @After
-    public void tearDown() throws IOException
-    {
-        if ( sparkContext != null )
-            sparkContext.close();
-
-        if ( Files.exists( Paths.get( basePath ) ) )
-            Assert.assertTrue( n5Supplier.get().remove() );
-    }
 
     @Test
     public void test1() throws IOException
@@ -80,7 +44,7 @@ public class N5ConnectedComponentsSparkTest
 
     private void runTest( final int[] blockSize ) throws IOException
     {
-        final N5Writer n5 = n5Supplier.get();
+        final N5Writer n5 = new N5FSWriter( basePath );
         final long[] dimensions = new long[] { 4, 4, 3 };
 
         final int[] data = {
@@ -104,7 +68,7 @@ public class N5ConnectedComponentsSparkTest
 
         N5ConnectedComponentsSpark.connectedComponents(
                 sparkContext,
-                n5Supplier,
+                () -> new N5FSWriter( basePath ),
                 datasetPath,
                 relabeledDatasetPath,
                 Optional.empty(),
